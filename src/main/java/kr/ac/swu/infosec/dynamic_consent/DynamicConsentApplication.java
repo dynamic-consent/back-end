3ca7024a727a7325a21b3f728f0d4d96894b3a18
dayeon053 <dayeon053@gmail.com>
2025-08-19 18:59:04 +0900

    feat: Add main API controller and security config

---
+++ src/main/java/kr/ac/swu/infosec/dynamic_consent/DynamicConsentApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.swu.infosec.dynamic_consent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicConsentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicConsentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/swu/infosec/dynamic_consent/DynamicConsentApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.swu.infosec.dynamic_consent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicConsentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
